10af2fa10900803c153add81548ddafb4fd73b0d
Alhineidi Ahmad (aa22x177) <aa22x177@submit01.ubelix.unibe.ch>
2025-04-08 14:39:30 +0200

    add basic pages

---
+++ src/pages/chat.py
@@ -0,0 +1,5 @@
+import ollama
+import streamlit as st
+
+st.title("chatbot")
+st.write("chatbot")
---
+++ src/pages/transcribe.py
@@ -0,0 +1,5 @@
+import whisper
+import streamlit as st
+
+st.title("transcribe audio!")
+st.write("Transcribe audio using openai whisper")
