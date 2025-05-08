72de269717f81e4f3b243f986240f2579b1d2e4a
Viktor Kovtun <viktor.kovtun@unibe.ch>
2025-05-08 19:25:28 +0200

    Some test.

--- src/Home.py
+++ src/Home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import os
 import base64
+import olmocr
 
 st.set_page_config(page_title="TEXT LAB", layout="wide")
 
@@ -15,7 +16,7 @@ def get_img_as_base64(file_path):
 def main():
     check_token()
 
-    st.title("TEXT LAB")
+    st.title("TEXT LAB: " + olmocr.pipeline)
 
     # Custom CSS for styling the logos.
     css = """
